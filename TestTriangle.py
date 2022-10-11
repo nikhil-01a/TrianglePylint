22dc19ef68e1973f48e608262ccf23d00d71f59b
nikhilparmar <nkumarg@stevens.edu>
2022-10-11 00:14:50 -0400

    updating

--- TestTriangle.py
+++ TestTriangle.py
@@ -16,19 +16,19 @@ class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
     def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+        self.assertEqual(classifyTriangle(3,4,5),'Right angled triangle.','3,4,5 is a Right triangle')
 
     def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+        self.assertEqual(classifyTriangle(5,3,4),'Right angled triangle.','5,3,4 is a Right triangle')
         
     def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+        self.assertEqual(classifyTriangle(1,1,1),'Equilateral triangle.','1,1,1 should be equilateral')
 
     def testNotAtriangle(self):
-        self.assertEqual(classifyTriangle(10,4,3),'NotATriangle','10,4,3 is not a triangle')
+        self.assertEqual(classifyTriangle(10,4,3),'Not a triangle.','10,4,3 is not a triangle')
 
     def testScaleneTriangles(self):
-        self.assertEqual(classifyTriangle(7,4,5),'Scalene','7,4,5 is a scalene triangle')
+        self.assertEqual(classifyTriangle(7,4,5),'Scalene triangle.','7,4,5 is a scalene triangle')
 
 if __name__ == '__main__':
     print('Running unit tests')
