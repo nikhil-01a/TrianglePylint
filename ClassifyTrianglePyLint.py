98ba04d41531fdb67bcda8c9314ee1eba1b1c1e5
nikhilparmar <nkumarg@stevens.edu>
2022-10-10 23:29:08 -0400

    modified

--- ClassifyTrianglePyLint.py
+++ ClassifyTrianglePyLint.py
@@ -9,7 +9,7 @@ it returns 'Not a triangle'.
 
 #Function to classify triangles
 def classify_triangle(a,b,c):
-    if a+b>=c and b+c>=a and c+a>=b:
+    if a+b>c and b+c>a and c+a>b:
         if a==b and b==c:
             return 'Equilateral triangle.'
         elif a==b or b==c or a==c:
